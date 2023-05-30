ece98077b8a3556f51c260e034940b951033d845
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-05-29 18:19:22 -0700

    Added mutations in Utils file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,66 @@
 import { gql } from "@apollo/client";
+
+// LOGIN_USER will execute the loginUser mutation.
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+// SAVE_BOOK will execute the saveBook mutation
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput) {
+        saveBook(input: $input) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+  }
+`;
+
+// REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+    mutation removeFriend($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
